d0fb2ae093139ca4ba4abeb16d4890e08b4a5725
Egor305 <Egor.chelak@gmail.com>
2013-08-30 01:26:17 +0300

    Making things look nicer (still a mess). Rewrite

--- ccbin.lua
+++ ccbin.lua
@@ -1,21 +1,24 @@
-tArgs = {...}
-if tArgs[1] == "get" then
-print "connecting to CCbin..."
-h = http.get("http://figgycity50.kd.io/ccbin/raw.php?id="..tArgs[2])
-paste = h.readAll()
-output = fs.open(tArgs[3], "w")
-output.write(paste)
-output.close()
-h.close()
-print ("Done! Saved at "..tArgs[3]..".")
-end
-if tArgs[1] == "put" then
-input = fs.open(tArgs[2], "r")
-paste = input.readAll()
-input.close()
-print "connecting to CCbin..."
-h = http.post("http://figgycity50.kd.io/ccbin/api.php", "name="..tArgs[3].."&paste="..paste)
-code = h.readAll()
-h.close()
-print ("Done! Saved in the cloud at http://figgycity50.kd.io/ccbin/?id="..code)
+local tArgs = {...}
+local baseurl = "http://figgycity50.kd.io/ccbin/"
+
+if tArgs[1] == "get" and #tArgs == 2 then
+  print "connecting to CCbin..."
+  local h = http.get(baseurl.."raw.php?id="..tArgs[2])
+  local fhFile = fs.open(shell.resolve(tArgs[3]), "w")
+  fhFile.write(h.readAll())
+  fhFile.close()
+  h.close()
+  print("Done! Saved at "..shell.resolve(tArgs[3])..".")
+
+elseif tArgs[1] == "put" and I#tArgs == 2 or #tArgs == 3) then
+  if not tArgs[3] then tArgs[3] == "Untitled"
+  local fhFile = fs.open(tArgs[2], "r")
+  print "connecting to CCbin..."
+  local h = http.post(baseurl.."api.php", "name="..tArgs[3].."&paste="..fhFile.readAll())
+  fhFile.close()
+  print ("Done! Saved in the cloud at "..baseurl.."?id="..h.readAll())
+  h.close()
+
+else
+  print("Usage: ccbin.lua <get <id> <filename>|put <filename> [title]>")
 end
