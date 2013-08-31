5c3bc245042edfd3da6fe7b146091ffbcd6304ad
Egor305 <Egor.chelak@gmail.com>
2013-08-31 17:32:46 +0300

    Added comments. Changed program name thing a bit

--- ccbin.lua
+++ ccbin.lua
@@ -1,13 +1,13 @@
 --Authors: figgycity50, Egor305
 local tArgs = {...}
-local sBaseURL = "http://figgycity50.kd.io/ccbin/"
+local sBaseURL = "http://figgycity50.kd.io/ccbin/" --Base URL. If hosting changes, change this.
 
-local sProgName = shell.getRunningProgram()
+local sProgName = shell.getRunningProgram() --Gets program's name, in case it's get renamed.
 local i = 0
 local j = 1
 while true do
-  i = sProgName:find("/",i+1)
-  if i == nil then
+  i = sProgName:find("/",j)
+  if not i then
     sProgName = sProgName:sub(j,-1)
     i = nil
     j = nil
@@ -16,7 +16,7 @@ while true do
   j = i + 1
 end
 
-if tArgs[1] == "get" and #tArgs == 3 then
+if tArgs[1] == "get" and #tArgs == 3 then --Get paste
   print "connecting to CCbin..."
   local h = http.get(sBaseURL.."raw.php?id="..tArgs[2])
   local fhFile = fs.open(shell.resolve(tArgs[3]), "w")
@@ -25,7 +25,7 @@ if tArgs[1] == "get" and #tArgs == 3 then
   h.close()
   print("Done! Saved at "..shell.resolve(tArgs[3])..".")
 
-elseif tArgs[1] == "put" and (#tArgs == 2 or #tArgs == 3) then
+elseif tArgs[1] == "put" and (#tArgs == 2 or #tArgs == 3) then --Put paste
   if not tArgs[3] then tArgs[3] = "" end
   local fhFile = fs.open(tArgs[2], "r")
   print "connecting to CCbin..."
@@ -35,5 +35,5 @@ elseif tArgs[1] == "put" and (#tArgs == 2 or #tArgs == 3) then
   h.close()
 
 else
-  print("Usages:\n"..sProgName.." get <id> <filename>\n"..sProgName.." put <filename> [title]")
+  print("Usages:\n"..sProgName.." get <id> <filename>\n"..sProgName.." put <filename> [title]") --Prints usage.
 end
