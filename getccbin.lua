8f64b509de68994cb5df721c39d0abd50d502406
Egor305 <Egor.chelak@gmail.com>
2013-08-31 23:05:11 +0300

    sometimes i forgot that this is lua

--- getccbin.lua
+++ getccbin.lua
@@ -1,5 +1,5 @@
 local tArgs = {...}
-if (http and #tArgs == 1)
+if http and #tArgs == 1
   print "connecting to GitHub..."
   local h = http.get("https://raw.github.com/Egor305/ccbin-client/master/ccbin.lua")
   local fhFile = fs.open(shell.resolve(tArgs[1]), "w")
@@ -8,6 +8,6 @@ if (http and #tArgs == 1)
   h.close()
   print("Done! Saved at "..shell.resolve(tArgs[1])..".")
 else
-  if (not http) then print("HTTP is off!!") end
+  if not http then print("HTTP is off!!") end
   print("Usage: getccbin.lua <filename>")
 end
