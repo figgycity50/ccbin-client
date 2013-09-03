06c589dc6dcc184c70b9debdfa87db18d8522c1b
Egor305 <Egor.chelak@gmail.com>
2013-09-03 19:36:42 +0300

    Refactoring a bit :) also, again forgot about "then"...

--- getccbin.lua
+++ getccbin.lua
@@ -1,13 +1,18 @@
 local tArgs = {...}
-if http and #tArgs == 1
-  print "connecting to GitHub..."
-  local h = http.get("https://raw.github.com/Egor305/ccbin-client/master/ccbin.lua")
-  local fhFile = fs.open(shell.resolve(tArgs[1]), "w")
-  fhFile.write(h.readAll())
-  fhFile.close()
-  h.close()
-  print("Done! Saved at "..shell.resolve(tArgs[1])..".")
-else
-  if not http then print("HTTP is off!!") end
-  print("Usage: getccbin.lua <filename>")
+if not http then
+  print("HTTP is off!!")
+  return nil
 end
+
+if not #tArgs == 1 then
+  print("Usage: getccbin.lua <filename>")
+  return nil
+end
+
+print "connecting to GitHub..."
+local h = http.get("https://raw.github.com/Egor305/ccbin-client/master/ccbin.lua")
+local fhFile = fs.open(shell.resolve(tArgs[1]), "w")
+fhFile.write(h.readAll())
+fhFile.close()
+h.close()
+print("Done! Saved at "..shell.resolve(tArgs[1])..".")
